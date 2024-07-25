7726259d01c29b5a7663da689a5262bdef776a29
Vanshika-bibipuria <vanshikabibipuria18@gmail.com>
2024-07-25 09:45:06 -0700

    Add files via upload

---
+++ evenodd.js
@@ -0,0 +1,7 @@
+let n=9;
+if(n%2==0){
+    console.log("its an even no , True")
+}
+else{
+    console.log("its a odd no,False")
+}
---
+++ ternary.js
@@ -0,0 +1,10 @@
+let x=7;
+let y=8;
+if(x>y){
+    console.log("x is greater");
+}
+else{
+    console.log("y is greater");
+}
+// using ternary operator
+printmax = x>y ? console.log("x is greater "):console.log("y is greater")
